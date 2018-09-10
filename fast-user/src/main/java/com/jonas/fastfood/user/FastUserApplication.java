efa0cd4e84a381633c0dd3be00481a4a1d435785
songjia <songjia@taiheiot.com>
2018-09-10 11:01:29 +0800

    commit

---
+++ fast-user/src/main/java/com/jonas/fastfood/user/FastUserApplication.java
@@ -0,0 +1,12 @@
+package com.jonas.fastfood.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastUserApplication.class, args);
+    }
+}
