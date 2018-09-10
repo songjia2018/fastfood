0442eea6a3f253264de9a9ef558bcde1e69556c5
songjia <songjia@taiheiot.com>
2018-09-10 11:08:50 +0800

    add

---
+++ fastfood-user/src/main/java/com/jonas/fastfood/user/FastfoodUserApplication.java
@@ -0,0 +1,12 @@
+package com.jonas.fastfood.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastfoodUserApplication.class, args);
+    }
+}
