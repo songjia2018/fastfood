846e62946ca17687bf08382940d6a9d22ee5e0ba
songjia <songjia@taiheiot.com>
2018-09-10 10:56:58 +0800

    Initial commit

---
+++ src/test/java/com/jonas/fastfood/FastfoodApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jonas.fastfood;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FastfoodApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
