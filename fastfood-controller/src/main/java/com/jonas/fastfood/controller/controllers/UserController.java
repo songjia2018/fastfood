31109eb64abc2f53d12b3eef60d19e2e48eb4622
songjia <songjia@taiheiot.com>
2018-09-12 14:49:18 +0800

    upd

--- fastfood-controller/src/main/java/com/jonas/fastfood/controller/controllers/UserController.java
+++ fastfood-controller/src/main/java/com/jonas/fastfood/controller/controllers/UserController.java
@@ -3,18 +3,14 @@ package com.jonas.fastfood.controller.controllers;
 import com.jonas.fastfood.commonservice.user.model.UserEntity;
 import com.jonas.fastfood.controller.model.request.UserReq;
 import com.jonas.fastfood.controller.utils.JsonResult;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@ApiModel(value = "User-用户")
+
+@Api(value="/UserController", tags="用户模块")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -33,40 +29,33 @@ public class UserController {
     }
 
     @ApiOperation(value="创建用户", notes="根据User对象创建用户")
-    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    //@ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @RequestMapping(value="", method=RequestMethod.POST)
     public JsonResult postUser(@RequestBody UserReq user) {
         System.out.print(user.getUserName());
         System.out.print(user.getPassword());
         return JsonResult.success();
     }
-     /*
+
     @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
     @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
     @RequestMapping(value="/{id}", method=RequestMethod.GET)
-    public User getUser(@PathVariable Long id) {
-        return users.get(id);
+    public JsonResult getUser(@PathVariable Long id) {
+        return JsonResult.success();
     }
 
+
     @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
-            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
-    })
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
     @RequestMapping(value="/{id}", method=RequestMethod.PUT)
-    public String putUser(@PathVariable Long id, @RequestBody User user) {
-        User u = users.get(id);
-        u.setName(user.getName());
-        u.setAge(user.getAge());
-        users.put(id, u);
-        return "success";
+    public JsonResult putUser(@PathVariable Long id, @RequestBody UserReq user) {
+        return JsonResult.success();
     }
 
     @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
     @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
     @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-    public String deleteUser(@PathVariable Long id) {
-        users.remove(id);
-        return "success";
-    }*/
+    public JsonResult deleteUser(@PathVariable Long id) {
+        return JsonResult.success();
+    }
 }
